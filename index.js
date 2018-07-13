55ce25fc17188231bbfdb41e10c69be97701e769
Lara Sanders <larasanders@gmail.com>
2018-07-13 19:30:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,22 +4,23 @@ function init() {
   var formTemplate = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
   var template = formTemplate
 
+  function handlebars() {
+    Handlebars.registerHelper('showDescription', function(description) {
+       return new Handlebars.SafeString('description="recipeDescription"')
+     })
+    Handlebars.registerHelper('showIngredient', function(ingredient) {
+       return new Handlebars.SafeString('<li name="ingredientsList">' + ingredient + '</li>')
+     })
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
+  Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML)
+  }
+
   document.getElementsByTagName("main")[0].innerHTML
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
 
-function handlebars() {
-  Handlebars.registerHelper('showDescription', function(description) {
-     return new Handlebars.SafeString('description="recipeDescription"')
-   })
-  Handlebars.registerHelper('showIngredient', function(ingredient) {
-     return new Handlebars.SafeString('<li name="ingredientsList">' + ingredient + '</li>')
-   })
-Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
-Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML)
-}
 
 function createRecipe() {
   var recipeTemplate = Handlebars.compile(document.getElementById("recipe-template").innerHTML);
