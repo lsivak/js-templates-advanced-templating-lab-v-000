eb417bdee1caad6938da325687447bcc4435428a
Lara Sanders <larasanders@gmail.com>
2018-07-13 13:37:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 function init() {
   //put any page initialization/handlebars initialization here
+  var formTemplate = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
+  var template = formTemplate
+
+  document.getElementsByTagName("main")[0].innerHTML
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
+
+var recipe = {
+  description: 'yummy chicken noodle soup',
+  ingredients: [
+    {quantity: "1 cup", name: 'chicken'},
+    {quantity: "3 nanoliters", name: 'stock'},
+    {quantity: "12", name: 'noodles'}
+  ]
+}
+
+function createRecipe() {
+
+  var name = document.getElementById("name")
+  var description = document.getElementById("recipeDescription")
+  var ingredients = document.getElementById("ingredients")
+
+  document.getElementsByTagName("main")[0].innerHTML += template
+
+}
