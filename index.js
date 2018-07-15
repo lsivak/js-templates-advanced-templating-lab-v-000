16fc7e034c2edd066df0088ac44a8c5a0c736bf7
Lara Sanders <larasanders@gmail.com>
2018-07-15 23:47:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ displayIngredient()
      })
   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
   Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML)
-
+debugger
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
