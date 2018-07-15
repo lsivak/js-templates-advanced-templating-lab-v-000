7c09599f1b2523ba71c8cd32d144218c4c4cffcb
Lara Sanders <larasanders@gmail.com>
2018-07-15 23:06:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,10 +11,11 @@ displayIngredient()
     Handlebars.registerHelper('displayIngredient', function(ingredient) {
        return new Handlebars.SafeString('<li name="ingredientsList">' + ingredient + '</li>')
      })
+   }
   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
   Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML)
 
-}
+
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 })
