2d0c6f9150cf41cd5b906117b7bf2af16194d9c9
Lara Sanders <larasanders@gmail.com>
2018-07-15 22:46:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,13 +28,13 @@ function createRecipe() {
   var name = document.getElementById("name").value
   var description = document.getElementById("description").value
   var ingredients = document.getElementById("ingredients").value
-  var ingredientsList = []
+  var recipeIngredients = []
 for(var i=0;i<ingredients.length;i++) {
   if(ingredients[i].value !== "") {
-    ingredientsList.push(ingredients[i].value)
+    recipeIngredients.push(ingredients[i].value)
   }
 }
-  var recipe = {name, ingredientsList, description}
+  var recipe = {name, recipeIngredients, description}
 
 return recipe
 }
