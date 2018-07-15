74f3936302ef4d33f26f2d92bd5e25e6101bd643
Lara Sanders <larasanders@gmail.com>
2018-07-15 23:08:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 function init() {
 
   //put any page initialization/handlebars initialization here
-  var formTemplate = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
-  var template = formTemplate
+  var formTemplate = document.getElementById("recipe-form-template").innerHTML
+  var template = Handlebars.compile(formTemplate)
+  document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
 displayIngredient()
 
 }
