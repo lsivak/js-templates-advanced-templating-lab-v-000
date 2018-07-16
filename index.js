33b1ced6f9f4f4e94b8edb1d07a9a2c57d86e595
Lara Sanders <larasanders@gmail.com>
2018-07-16 13:42:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,14 +29,14 @@ function createRecipe() {
   var name = document.getElementById("name").value
   var description = document.getElementById("description").value
   var ingredients = document.getElementsByName("ingredients")
-  var recipeIngredients = []
+  var ingredientsList = []
   for(var i=0;i<ingredients.length;i++) {
     if(ingredients[i].value !== "") {
-      recipeIngredients.push(ingredients[i].value)
+      ingredientsList.push(ingredients[i].value)
   }
 }
 
-  var recipe = {name, recipeIngredients, description}
+  var recipe = {name, ingredientsList, description}
 return recipe
 
 }
@@ -48,13 +48,13 @@ function updateRecipe() {
   var name = document.getElementById("name").value
   var description = document.getElementById("description").value
   var ingredients = document.getElementsByName("ingredients")
-  var recipeIngredients = []
+  var ingredientsList = []
   for(var i=0;i<ingredients.length;i++) {
     if(ingredients[i].value !== "") {
-      recipeIngredients.push(ingredients[i].value)
+      ingredientsList.push(ingredients[i].value)
   }
 }
-  var recipe = {name, recipeIngredients, description}
+  var recipe = {name, ingredientsList, description}
 
 return recipe
 }
